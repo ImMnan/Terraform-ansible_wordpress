221ad4cf49f941ab41300f81d1f81f2bdd136174
Manan <immnan333@gmail.com>
2022-09-22 15:00:58 +0530

    security fix

--- main.tf
+++ main.tf
@@ -1,49 +1,59 @@
+/* Start with adding the local variables, which will be used throughout the script ami-id of the instance (see [2.5]),
+ use VPS (see [2.3]), key_name (see [2.2]) and key_path (see [2.2]) */
+
 locals {
   ami_id = "ami-09e67e426f25ce0d7"
-  vpc_id = "vpc-01b7844d367765b11"
+  vpc_id = "[vpc id]"
   ssh_user = "ubuntu"
   key_name = "wpkey"
   private_key_path = "/home/labsuser/wpkey.pem"
 }
 
+/* Declare the provider and other required information linked with it, access key, secret key and token as per AWS 
+(Or any cloud provider you are using)(See [2.4]) */
+
 provider "aws" {
   region     = "us-east-1"
-  access_key = "ASIAVSUEUSV4NRO32M2X"
-  secret_key = "HJo+iPpdITfttFjsSkl8kKHrMY+9ganEHNMCIhFo"
-  token = "FwoGZXIvYXdzEO3//////////wEaDLuiwxaQ4DxkZ4HK2iK0ATs2lcDG60oTOOs41tQ158PHb6hK3igCLTWZimtHwQn1C71Igph9rt9z0s5OKeoUT/VEp2IUa5SKe/KAc0T720PbOqT02WgFkT1zxpgXBxIJ3f35kVlVlpdpeUP3qNnPAkeU4AZAE6Rm5CCZdTAhfR4e2piLnLwQUwTL5nTiEZcS3bkF+wmQOBiJCCUS6Hsj8wODYsiDYEIUxHDx24Yx5ylM55mYYVNXfBsKPDFZBofpVSQb+CjWrq+ZBjItWoMRM/NigHN4c8yWv4Ms0cFNtxWtVaJroWvsjefRDAvffJFCUma23yEBp/cE"
+  access_key = "[AWS access key]"
+  secret_key = "[AWS secret key]"
+  token = "[AWS token]"
 }
+
+/* Creating a security group with the name of wpaccess and setting ingress egress security rules, it will automatically use the vac id from variables declared in local. */
 
 resource "aws_security_group" "wpaccess" {
-	name   = "wpaccess"
-	vpc_id = local.vpc_id
+  name   = "wpaccess"
+  vpc_id = local.vpc_id
     
   ingress {
-		from_port   = 22
-		to_port     = 22
-		protocol    = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   ingress {
-		from_port   = 80
-		to_port     = 80
-		protocol    = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
-		from_port   = 443
-		to_port     = 443
-		protocol    = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
-		from_port   = 0
-		to_port     = 0
-		protocol    = "-1"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
+# Resource creation using local variables - ami, security group and key. 
 
 resource "aws_instance" "web" {
   ami = local.ami_id
@@ -56,6 +66,9 @@ resource "aws_instance" "web" {
     Name = "wp ec2"
   }
 
+
+  /* Setting up connection as we want to use ssh for Ansible configurations to run. Again using local variables for host ip, user name, and security key */
+
   connection {
     type = "ssh"
     host = self.public_ip
@@ -64,21 +77,33 @@ resource "aws_instance" "web" {
     timeout = "4m"
   }
 
+
+# Just to confirm whether our remote access is working
+
   provisioner "remote-exec" {
     inline = [
       "hostname"
     ]
   }
 
+
+/* copying the remote machine ip to our local machine into my hosts file using local-exec */
+
   provisioner "local-exec" {
     command = "echo ${self.public_ip} > myhosts"
   }
+
+
+/* Running Ansible-playbook command through our local machine, using the myhosts as inventory file, ubuntu as user and wbkey.pem as private key. All satisfied using local variables */
 
   provisioner "local-exec" {
     command = "ansible-playbook -i myhosts --user ${local.ssh_user} --private-key ${local.private_key_path} initiate.yml"
   }
 
 }
+
+
+# Saving the output.
 
 output "instance_ip" {
   value = aws_instance.web.public_ip
